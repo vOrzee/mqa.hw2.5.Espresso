ff5098349e6eccccf4dce1a04c99452bb237cfe4
Roman Leshin <vorzee@gmail.com>
2025-06-28 17:37:32 +0500

    HW6. Task 1

--- app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/MainActivityTest.java
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/MainActivityTest.java
@@ -1,19 +1,28 @@
 package ru.kkuzmichev.simpleappforespresso;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasAction;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasData;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import android.content.Intent;
+
 import androidx.test.espresso.ViewInteraction;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.espresso.intent.Intents;
 import androidx.test.rule.ActivityTestRule;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-@RunWith(AndroidJUnit4.class)
+import io.qameta.allure.android.runners.AllureAndroidJUnit4;
+
+@RunWith(AllureAndroidJUnit4.class)
 public class MainActivityTest {
     @Rule
     public ActivityTestRule<MainActivity> activityTestRule =
@@ -27,5 +36,19 @@ public class MainActivityTest {
         mainText.check(
                 matches(withText("This is home fragment"))
         );
+    }
+
+    @Test
+    public void testPropertyIntent() {
+        ViewInteraction overflowMenuButton = onView(withContentDescription("More options"));
+        overflowMenuButton.perform(click());
+
+        ViewInteraction materialTextView = onView(withText("Settings"));
+
+        Intents.init();
+        materialTextView.perform(click());
+        intended(hasData("https://google.com"));
+        intended(hasAction(Intent.ACTION_VIEW));
+        Intents.release();
     }
 }
